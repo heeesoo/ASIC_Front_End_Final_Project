d76a8ca07024e9770bcac7580e6f8fd2344ec557
Physical Eng. <132325298+heeesoo@users.noreply.github.com>
2026-02-12 21:36:06 +0900

    Add files via upload

---
+++ sim/pre/run_pre.f
@@ -0,0 +1,6 @@
+// Library
+-v /tools/dk/SAED32_EDK/lib/stdcell_rvt/verilog/saed32nm.v
+
+../../syn/2_Output/mapped/fir_filter.v
+
+../tb/tb_fir_filter.v
---
+++ sim/pre/sim_pre.tcl
@@ -0,0 +1,1 @@
+Verdi-Ultra -f run_pre.f -ssf test.pre.fsdb &
